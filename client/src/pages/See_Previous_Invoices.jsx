e204737024d82670609b2fc01aaf9ac482b6dae7
iker7080 <iker7080@gmail.com>
2024-10-29 19:31:04 -0500

    Update See_Previous_Invoices.jsx

--- client/src/pages/See_Previous_Invoices.jsx
+++ client/src/pages/See_Previous_Invoices.jsx
@@ -4,9 +4,10 @@ import axios from 'axios';
 import './style.css';
 
 
-const See_Patient_Balance = () => {
+const See_Previous_Invoices = () => {
     const [patient, setPatient] = useState('');
     const navigate = useNavigate();
+    const choice = (localStorage.getItem('choice') === 'true');
 
     
     useEffect(() => {
@@ -19,58 +20,30 @@ const See_Patient_Balance = () => {
         }
     }, []);
 
-    const setTotal = () =>{
-        var sum = 0;
-            for(var i =0; i < patient.length; i++){
-                sum += patient[i].cost;
-            }
-            return sum;
-    }
-
-    
-
     const handleLogout = () => {
         localStorage.removeItem('patient'); // Clear employee info
         navigate('/Billing_Staff_View/SearchPatient'); // Navigate to the main page
     };
 
-
-    const handleButtonClick = () => {
-        const userConfirmed = window.confirm('Are you sure you want to proceed?');
-    
-        if (userConfirmed) {
-          // The user clicked "OK"
-          return true;
-        } else {
-          // The user clicked "Cancel"
-          return false;
-
-        }
-    };
-   
     if (!patient) {
         return <div>No patient information found.</div>;
     }
 
     const handleonClick = async (index) =>{
-       if(!handleButtonClick()){
-            return;
-       }
         localStorage.setItem('single_appointment', JSON.stringify(patient[index]));
         const ID = patient[index].appointment_ID;
 
         console.log("app index: ", ID);
-        try{
-            await axios.put(`http://localhost:3000/See_Patient_Balance`, {ID});
-            await axios.post(`http://localhost:3000/See_Patient_Balance`, {ID});
-            console.log("office ID: ",patient[index].officeID);
 
+        try{
+            console.log(patient[index].officeID);
             const offID = patient[index].officeID;
-            const res = await axios.post(`http://localhost:3000/Created_invoice`, {offID});
+            const res = await axios.post(`http://localhost:3000/Created_invoice`, {offID, choice});
             localStorage.setItem('office_loc', JSON.stringify(res.data));
             console.log("office retrieved: ",res.data);
+
+
         }catch(e){
-            console.log("catched");
             console.log(e);
             return;
         }
@@ -82,7 +55,7 @@ const See_Patient_Balance = () => {
 try{
     return (
         <div>
-            <h1>Unpaid Bills</h1>
+            <h1>Paid Invoices</h1>
             <p>ID: {patient[0].medical_ID}</p>
             <p>Name: {patient[0].first_name} {patient[0].last_name}</p>
 
@@ -93,7 +66,7 @@ try{
                             <th>Appointment ID</th>
                             <th>Date and Time</th>
                             <th>Doctor</th>
-                            <th>Cost</th>
+                            <th>Payed On</th>
                             <th>Action</th>
                         </tr>
                     </thead>
@@ -103,10 +76,10 @@ try{
                                 <td>{patient.appointment_ID}</td>
                                 <td>{patient.dateTime}</td>
                                 <td>{patient.doctor}</td>
-                                <td>$ {patient.cost}</td>
+                                <td>{patient.issuedDate}</td>
                                 <td>
                                     <button onClick={() => handleonClick(index)}>
-                                        Pay
+                                        See Invoice
                                     </button>
                                 </td>
                             </tr>
@@ -115,10 +88,7 @@ try{
                     </tbody>
                 </table>
             </div>
-
-                <h4>Total Due: $ {setTotal()}</h4>
-
-
+            <br />
             <button className = "logout" onClick={handleLogout}>Return</button>
 
         </div>
@@ -137,6 +107,12 @@ try{
     
 
 }
+    
+};
+
+
+
+export default See_Previous_Invoices;
     
 };
 
