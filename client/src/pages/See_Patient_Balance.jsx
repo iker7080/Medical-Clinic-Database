8d0f4b98a9df2dbcd149d24a86e2236a17eadb97
iker7080 <iker7080@gmail.com>
2024-11-07 10:59:19 -0600

    Update See_Patient_Balance.jsx

--- client/src/pages/See_Patient_Balance.jsx
+++ client/src/pages/See_Patient_Balance.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './style.css';
+import './billing_staff_invoice.css';
 
 
 const See_Patient_Balance = () => {
@@ -22,7 +22,7 @@ const See_Patient_Balance = () => {
     const setTotal = () =>{
         var sum = 0;
             for(var i =0; i < patient.length; i++){
-                sum += patient[i].cost;
+                sum += patient[i].amountDue;
             }
             return sum;
     }
@@ -60,16 +60,11 @@ const See_Patient_Balance = () => {
         const ID = patient[index].appointment_ID;
 
         console.log("app index: ", ID);
-        console.log(patient[index].dateTime);
-        const correctdate= patient[index].dateTime.toString().slice(0, 19).replace('T', ' ');
 
         try{
-            const q = `INSERT INTO Invoice (appointment_ID, appointmentDateTime, patientBillingID, patient_name, patient_address, patient_phone, patient_email, patient_insurance, services, amountCharged, amountDue, created ) VALUES ('`+patient[index].appointment_ID+`','`+correctdate+`','`+patient[index].billingID+`','`+patient[index].first_name+' '+patient[index].last_name+`','`+patient[index].address_line_1+`','3332224552','johndoe@example.com','Insurance Company ABC','`+patient[index].appointment_type+`',`+patient[index].cost+`,0.00, CURRENT_TIMESTAMP);`;
 
-    console.log(q);
             
-            await axios.put(`http://localhost:3000/See_Patient_Balance`, {ID});
-            await axios.post(`http://localhost:3000/See_Patient_Balance`, {q});
+            await axios.post(`http://localhost:3000/See_Patient_Balance`, {ID});
             console.log("office ID: ",patient[index].officeID);
 
             const offID = patient[index].officeID;
@@ -89,9 +84,9 @@ const See_Patient_Balance = () => {
 try{
     return (
         <div>
-            <h1>Unpaid Bills</h1>
-            <p>ID: {patient[0].medical_ID}</p>
-            <p>Name: {patient[0].first_name} {patient[0].last_name}</p>
+            <h1>Unpaid Bills</h1> 
+            <p>ID: {patient[0].patientmedicalID}</p>
+            <p>Name: {patient[0].patientName}</p>
 
             <div className='invoiceList'>
                 <table className='invoicetable'>
@@ -99,7 +94,6 @@ try{
                         <tr>
                             <th>Appointment ID</th>
                             <th>Date and Time</th>
-                            <th>Doctor</th>
                             <th>Cost</th>
                             <th>Action</th>
                         </tr>
@@ -108,9 +102,8 @@ try{
                         {patient.map((patient, index) => (
                                 <tr key ={patient.appointment_ID}>
                                 <td>{patient.appointment_ID}</td>
-                                <td>{patient.dateTime}</td>
-                                <td>{patient.doctor}</td>
-                                <td>$ {patient.cost}</td>
+                                <td>{patient.appointmentDateTime}</td>
+                                <td>$ {patient.amountDue}</td>
                                 <td>
                                     <button onClick={() => handleonClick(index)}>
                                         Pay
@@ -131,7 +124,8 @@ try{
         </div>
     );
 
-}catch{
+}catch(e){
+    console.log(e);
     return(
     <div>Patient has no ammounts due
         <div>
