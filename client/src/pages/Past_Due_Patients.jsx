8c254012c40cdc1c5962a868d6c582f614220622
iker7080 <iker7080@gmail.com>
2024-11-17 00:11:55 -0600

    Update Past_Due_Patients.jsx

--- client/src/pages/Past_Due_Patients.jsx
+++ client/src/pages/Past_Due_Patients.jsx
@@ -2,87 +2,177 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import './style.css';
+import { Line } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    ChartData, 
+    ScatterDataPoint,
+    CategoryScale,
+    LinearScale,
+    LineElement,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+    ArcElement,
+    PointElement,
+} from 'chart.js';
+
+ChartJS.register(CategoryScale, LinearScale, LineElement, BarElement, Title, Tooltip, Legend, ArcElement, PointElement);
 
 
 const Past_Due_Patients = () => {
-    const [patients, setPatients] = useState('');
+    const [patients, setPatients] = useState([]);
+    const [officeData, setOfficeData] = useState([]);
+    const [filterDateRange, setFilterDateRange] = useState({ start: '', end: '' });
     const navigate = useNavigate();
+    const [lineChartData, setLineChartData] = useState({});
+
 
     
     useEffect(() => {
-        const patients_Data = localStorage.getItem('Past_Due_Patients');
-        console.log('Retrieved patients data:', patients_Data); // Add this line for debugging
-        if (patients_Data) {
-            setPatients(JSON.parse(patients_Data));
-        }else{
-            console.log("noai");
+        fetchPatientStatistics();
+    }, [filterDateRange]);
+
+    const fetchPatientStatistics = async () =>{
+        if (!filterDateRange.start || !filterDateRange.end) {
+            console.error('Start date and end date must be provided.');
+            return;
         }
-    }, []);
+
+        try {
+            const res = await axios.post(`http://localhost:3000/Past_Due_Patients`, {
+                params: {
+                    startDate: filterDateRange.start,
+                    endDate: filterDateRange.end,
+                },
+            });
+
+            setPatients(res.data);
+            console.log("past due patient data:",res.data);
+            prepareLineChartData(res.data);
+        } catch (error) {
+            console.error('Error fetching patient statistics:', error);
+        }
+    }
+
+    const colors = [
+        'rgba(75, 192, 192, 0.6)', 
+        'rgba(255, 99, 132, 0.6)', 
+        'rgba(255, 206, 86, 0.6)', 
+        'rgba(54, 162, 235, 0.6)', 
+        'rgba(153, 102, 255, 0.6)', 
+        'rgba(255, 159, 64, 0.6)', 
+    ];
+
+    const prepareLineChartData = (data) => {
+        const labels = data.map((item) => new Date(item.appointmentDateTime).toLocaleDateString(
+            'en-US', { month: 'short', day: 'numeric', year: 'numeric'}
+        ));
+        const amountDue = data.map((item) => item.amountDue);
+
+        setLineChartData({
+            labels,
+            datasets: [
+                {
+                    label: 'Total Debt',
+                    data: amountDue,
+                    backgroundColor: 'rgba(75, 192, 192, 0.6)',
+                    borderColor: 'rgba(75, 192, 192, 0.6)',
+                    borderWidth: 2,
+                    pointBorderColor:'rgba(75, 192, 192, 0.6)',
+                    spanGaps : true,
+                    fill : false
+                },
+            ],
+        });
+    };
 
     const handleLogout = () => {
         localStorage.removeItem('Past_Due_Patients'); // Clear employee info
         navigate('/Billing_Staff_View'); // Navigate to the main page
     };
 
-
+    const chartOptions = {
+        responsive: true,
+        scales: { 
+            x: {
+                 title: { 
+                    display: true, 
+                    text: 'Date', 
+                }, 
+            }, 
+            y: { 
+                 title: { 
+                    display: true, 
+                    text: 'Total Amount Due', }, },}
+    
+    }
     
 
-try{
-    if(patients.length == 0){
-        throw("No patients retrieved");
-
-    }
     return (
         <div>
-            <h1>Past Due Patients</h1>
+            <h1>Due Invoices Report</h1>
 
-            <div className='invoiceList'>
-                <table className='invoicetable'>
-                    <thead>
-                        <tr>
-                            <th>Patient ID</th>
-                            <th>Name</th>
-                            <th>email</th>
-                            <th>Home Number</th>
-                            <th>Work Number</th>
-                            <th>Cell Number</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {patients.map(patient => (
-                                <tr key ={patient.billingID}>
-                                <td>{patient.billingID}</td>
-                                <td>{patient.first_name} {patient.last_name}</td>
-                                <td className='table-cell'>{patient.personal_email}</td>
-                                <td>{patient.home_phone} </td>
-                                <td>{patient.work_phone} </td>
-                                <td>{patient.cell_phone} </td>
-                            </tr>
-                        ))}
-
-                    </tbody>
-                </table>
+            <div className="Report-container">
+                    <div className="Report-left">
+                        <label className="os_label">
+                            <h3 className="os_h3">Start Date</h3>
+                            <input className="os_input-date" type="date" onChange={(e) => setFilterDateRange({ ...filterDateRange, start: e.target.value })} />
+                        </label>
+                        <label className="os_label">
+                            <h3 className="os_h3">End Date</h3>
+                            <input className="os_input-date" type="date" onChange={(e) => setFilterDateRange({ ...filterDateRange, end: e.target.value })} />
+                        </label>
+                        <button className="add" onClick={fetchPatientStatistics}>Filter</button>
+                    </div>
+                    <div className="Report-right">
+                        {lineChartData.labels && lineChartData.labels.length > 0 ? (
+                            <Line data={lineChartData} options = {chartOptions} />
+                        ) : (
+                            <p>No data available for the selected filters.</p>
+                        )}
+                </div>
             </div>
+
+                
+
+
+            
+            <br />
+
+            <table  className='invoicetable'>
+                <thead className='invoicetable-cell'>
+                    <tr>
+                        <th>Patient ID</th>
+                        <th>Patient Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Unpaid invoice creation</th>
+                        <th>amount Due</th>
+                    </tr>
+                </thead>
+                <tbody className='invoicetable-cell'>
+                    {patients.map((patient) => (
+                        <tr key = {patient.appointmentDateTime}>
+                            <td>{patient.patientBillingID}</td>
+                            <td>{patient.patient_name}</td>
+                            <td>{patient.personal_email}</td>
+                            <td>{patient.home_phone}</td>
+                            <td>{patient.appointmentDateTime}</td>
+                            <td>$ {patient.amountDue}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
             <br />
             <button className = "logout" onClick={handleLogout}>Return</button>
 
+
         </div>
     );
 
-}catch (err){
-    console.log(err);
-    return(
-    <div>No Patients with Unpaid Bills in the past 2 weeks
-        <div>
-            <button className = "logout" onClick={handleLogout}>Return</button>
-        </div>
-    </div>
-    );
 
-    
-    
-
-}
     
 };
 
